d6961633361217ce5dc8e44fe5119ddaede29a20
vannt010391 <vannt.sptb@gmail.com>
2019-03-22 21:50:43 +0700

    Change helloworld file format to helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,6 @@
-#Writing my first python, and ofcourse begining with Hello World :)
+# Writing my first python, and ofcourse begining with Hello World :)
 print("I am new to python. Hello, python world")
-van="I am going to begin this python journey with my friend Van"
+van = "I am going to begin this python journey with my friend Van"
 print(van)
+vicky = " I've just change your file format to helloworld.py"
+print(vicky)
